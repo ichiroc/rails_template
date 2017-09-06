9aefe40c8aa2682a2571e073e102b0b355a2f874
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-09-06 13:43:39 +0900

    rubocop

--- template.rb
+++ template.rb
@@ -6,7 +6,7 @@ def windows?
 end
 
 has_user = yes? 'Do you want to add devise, rolify, pudint gems? [y/n]'
-if has_user and yes? 'Do you want to add devise model right now? [y/n]'
+if has_user && yes?('Do you want to add devise model right now? [y/n]')
   devise_model = ask 'Type devise model name (DON\'T INCLUDE OPTIONS) : [User]'
   devise_model = 'User' if devise_model.blank?
   devise_model_options = ask "Type devise model options : rails g devise #{devise_model}"
@@ -32,7 +32,7 @@ gem_group :development, :test do
   gem 'database_cleaner'
   gem 'factory_girl_rails'
   gem 'faker'
-  gem "font-awesome-rails"
+  gem 'font-awesome-rails'
   gem 'guard-rspec'
   gem 'launchy'
   gem 'letter_opener'
@@ -45,7 +45,7 @@ end
 # Because it will fail install, skip these gems if it run on Windows.
 unless windows?
   gem 'therubyracer'
-  gem "less-rails"
+  gem 'less-rails'
 end
 
 run 'bundle install'
@@ -56,7 +56,7 @@ generate 'annotate:install'
 generate 'rspec:install'
 
 # Uncomment Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f } in spec/rails_helper.rb
-uncomment_lines 'spec/rails_helper.rb', /Dir.*spec\/support\/.*/
+uncomment_lines 'spec/rails_helper.rb', %r{Dir.*spec/support/.*}
 
 inject_into_file 'spec/rails_helper.rb', before: "\nend\n" do
   "\n  config.include FactoryGirl::Syntax::Methods\n"
@@ -67,29 +67,27 @@ run 'yarn add jquery'
 scss_file = 'app/assets/stylesheets/application.scss'
 run "mv 'app/assets/stylesheets/application.css' #{scss_file}"
 
-inject_into_file "app/assets/stylesheets/application.scss", after: '*/' do
+inject_into_file 'app/assets/stylesheets/application.scss', after: '*/' do
   "\n@import 'bootstrap';"
 end
 
 gsub_file(scss_file, /\*= require.*\n/, '')
 
-
-inject_into_file "#{'app/assets/javascripts/application.js'}", before: '//= require_tree .' do
+inject_into_file 'app/assets/javascripts/application.js', before: '//= require_tree .' do
   <<~JS
-  //= require jquery
-  //= require popper
-  //= require bootstrap-sprockets
+    //= require jquery
+    //= require popper
+    //= require bootstrap-sprockets
   JS
 end
 
 if has_user
   generate 'devise:install'
-  environment %q(config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }), env: 'development'
+  environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }", env: 'development'
   generate "devise #{devise_model} #{devise_model_options}" if devise_model
   generate 'pundit:install'
   generate "rolify Role #{devise_model}"
 end
-
 
 rake 'erb2haml'
 rails_command 'db:migrate' if yes? 'Migrate db?'
